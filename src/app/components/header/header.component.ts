532f06d05e7fb8151cc158cabda17c70fe9ce5be
unknown <saad.lmgharni@etu.umontpellier.fr>
2023-12-06 03:27:28 +0100

    2nd try

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { HostListener } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  public showAccountMenu = false;
 
-  constructor() { }
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event): void {
+    // Effectuez une assertion de type pour 'target' comme un élément HTML
+    const target = event.target as HTMLElement;
 
-  ngOnInit(): void {
+    // Si le clic est en dehors du menu, cachez-le
+    if (target && !target.matches('.compte, .compte *')) {
+      this.showAccountMenu = false;
+    }
   }
 
+  public toggleAccountMenu(event: MouseEvent): void {
+    // Empêche le clic de se propager au document
+    event.stopPropagation();
+    this.showAccountMenu = !this.showAccountMenu;
+  }
 }
